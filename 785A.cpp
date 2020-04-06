f0474e22268abf18fa288679b65401b3bb962429
AshrafulMorsalinSizan99 <50795768+AshrafulMorsalinSizan99@users.noreply.github.com>
2020-04-06 14:24:43 +0600

    Add files via upload

---
+++ 785A.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long int n,i,s=0;
+    string a;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>a;
+        if(a[0]=='T')
+        {
+            s+=4;
+        }
+        else if(a[0]=='C')
+        {
+            s+=6;
+        }
+        else if(a[0]=='O')
+        {
+            s+=8;
+        }
+        else if(a[0]=='D')
+        {
+            s+=12;
+        }
+        else if(a[0]=='I')
+        {
+            s+=20;
+        }
+    }
+    cout<<s<<endl;
+    return 0;
+}
