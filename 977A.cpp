b0c9e999176b8832d5f0483ed1f2352b623c15fa
AshrafulMorsalinSizan99 <50795768+AshrafulMorsalinSizan99@users.noreply.github.com>
2020-04-10 18:16:41 +0600

    Add files via upload

---
+++ 977A.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b,i;
+    cin>>a>>b;
+    for(i=1;i<=b;i++)
+    {
+        if(a%10==0)
+        {
+            a=a/10;
+        }
+        else
+        {
+            a--;
+        }
+    }
+    cout<<a<<endl;
+    return 0;
+}
